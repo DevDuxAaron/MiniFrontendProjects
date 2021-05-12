3822c05f4a1f00f86bcacf5f2865f28ee18eaa10
Aaron <relojvale25@gmail.com>
2021-05-11 20:46:26 -0400

    Expandable menu begginer

---
+++ expandingMenu/app.js
@@ -0,0 +1,12 @@
+let state = false
+const expand = () => {
+    if (!state) {
+        document.getElementById("items").style.transform = 'scaleX(1)'
+        document.getElementById("toggle").style.transform = 'rotate(45deg)'
+        state = true
+    } else {
+        document.getElementById("items").style.transform = 'scaleX(0)'
+        document.getElementById("toggle").style.transform = 'rotate(0deg)'
+        state = false
+    }
+}
